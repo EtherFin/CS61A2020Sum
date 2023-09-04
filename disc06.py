e50e767ecb3cc474cac022169175dafca253d628
Fin <112712156+EtherFin@users.noreply.github.com>
2023-09-04 20:59:08 +0800

    update the disc06.py

--- disc06.py
+++ disc06.py
@@ -240,5 +240,5 @@ def subset_sum(seq, k):
         return any([subset_sum(seq[1:], k - x) for x in seq])
 
 
-print(subset_sum([2, 4, 7, 3], 5))
-print(subset_sum([1, 9, 5, 7, 3], 2))
+# print(subset_sum([2, 4, 7, 3], 5))
+# print(subset_sum([1, 9, 5, 7, 3], 2))
