8e30f151ffca9345f1b4ae38e3e9bd8df9450d44
Fin <112712156+EtherFin@users.noreply.github.com>
2023-09-14 16:59:10 +0800

    half finish the disc08.py

--- disc08.py
+++ disc08.py
@@ -19,13 +19,13 @@ a = A()
 b = B(1)
 b.n = 5
 
-# C(2).n
+C(2).n
 
 
-# a.z == C.z
+a.z == C.z
 
 
-# a.z == b.z
+a.z == b.z
 
 
 # Which evaluates to an integer?
@@ -34,3 +34,87 @@ b.n = 5
 print(b.z.z.z)
 # b.z.z.z.z
 # None of these
+
+print('hello?')
+
+def generate_subsets():
+	"""
+	>>> subsets = generate_subsets()
+	>>> for _ in range(3):
+	... print(next(subsets))
+	...
+	[[]]
+	[[], [1]]
+	[[], [1], [2], [1, 2]]
+	"""
+	def helper(n):
+		if n == 0:
+			return [[]]
+		else:
+			return [[n] + elem for elem in helper(n-1)] + helper(n-1)
+	i = 0
+	while 1:
+		yield helper(i)
+		i+=1
+
+# print('hello!?')
+# subsets = generate_subsets()
+# for _ in range(3):
+# 	print(next(subsets))
+
+
+def sum_paths_gen(t):
+	"""
+	>>> t1 = tree(5)
+	>>> next(sum_paths_gen(t1))
+	5
+	>>> t2 = tree(1, [tree(2, [tree(3), tree(4)]), tree(9)])
+	>>> sorted(sum_paths_gen(t2))
+	[6, 7, 10]
+	"""
+	# if ___________________________:
+	# 	yield ____________________
+	# for __________________________:
+	# 	for __________________________:
+	# 		yield ____________________
+	if is_leaf(t):
+		yield label(t)
+	for branch in branches(t):
+		for s in sum_paths_gen(branch):
+			yield label(t) + s
+
+# Constructor
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    for branch in branches:
+        assert is_tree(branch)
+    return [label] + list(branches)
+
+# Selector
+def label(tree):
+    """Return the label value of a tree."""
+    return tree[0]
+
+# Selector
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    return tree[1:]
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+# For convenience
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise."""
+    return not branches(tree)
+
+# t1 = tree(5)
+# print(next(sum_paths_gen(t1)))
+# t2 = tree(1, [tree(2, [tree(3), tree(4)]), tree(9)])
+# print(sorted(sum_paths_gen(t2)))
